cc4de2672c1b8762bbb6b0e856fcc6875fb2a98e
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2025-01-26 18:47:25 +0500

    faf

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import Layout from './pages/Layout.jsx'
@@ -10,28 +10,28 @@ import Menus from './pages/Menus.jsx'
 import AddMenuItem from './pages/AddMenuItem.jsx'
 import { MenusProvider } from './context/Items.jsx'
 import { RatingsProvider } from './context/Ratings.jsx'
-import Anaylatics from './pages/Anaylatics.jsx'
 import Discussion from './pages/Anaylatics.jsx'
 import About from './pages/About.jsx'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
   <RatingsProvider>
     <MenusProvider>
-  <StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Layout/>}>
-        <Route index element={<Home/>}/>
-        <Route path='discussions' element={<Discussion/>}/>
-        <Route path='gallery' element={<Gallery/>}/>
-        <Route path='explore' element={<Menus/>}/>
-        <Route path='/:path/addItem' element={<AddMenuItem/>} />
-        <Route path='about' element={<About/>}/>
-      </Route>
-    </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+      <StrictMode>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path='discussions' element={<Discussion />} />
+              <Route path='gallery' element={<Gallery />} />
+              <Route path='explore' element={<Menus />} />
+              <Route path='/:path/addItem' element={<AddMenuItem />} />
+              <Route path='about' element={<About />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </StrictMode>
     </MenusProvider>
-    </RatingsProvider>
+  </RatingsProvider>
 )
